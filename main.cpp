030efc879a3b8581d67937ddd7b857402870ec7c
accacca44 <47551816+accacca44@users.noreply.github.com>
2019-02-18 21:36:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,60 @@
-//...C++...     +3x+2=+8+x
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <algorithm>
 
 using namespace std;
-int main()
-{
+int oldal;
+float tomb[2][2];
+void osszegek(string sztringek);
 
-    string egyenlet,elsoResz,masodikResz,szabadtag;
+int main(){
+
+    string egyenlet;
+    string elsoResz;
+    string masodikResz;
+    string szabadtag;
+
     ifstream book("Untitled1");
     int n;cin>>n;
 
-    for(int a=0; a<n; a++)
-    {
+    for(int a=0; a<n; a++){
+        oldal=0;
         getline(book,egyenlet);
-        cout<<egyenlet<<endl;
+        cout<<egyenlet<<" -->";
         int equ = egyenlet.find('=');
-        cout << "Az egyenlõ a(z) " << equ+1 << ". helyen van." << endl;
         elsoResz = egyenlet.substr(0,equ);
         masodikResz = egyenlet.substr(equ+1);
-        cout <<  elsoResz << " " << masodikResz<<endl;
 
+        osszegek(elsoResz);
+
+        oldal++;
+        osszegek(masodikResz);
+
+
+        if(tomb[0][0]==tomb[1][0] && tomb[0][1]==tomb[1][1])cout<<"Vegtelen megoldas van!"<<endl;
+        if(tomb[0][0]==tomb[1][0] && tomb[0][1]!=tomb[1][1])cout<<"Nincs valos megoldas!"<<endl;
+        if(tomb[0][0]!=tomb[1][0]){
+            float answ;
+            answ = (tomb[1][1]-tomb[0][1])/(tomb[0][0]-tomb[1][0]);
+            cout<<"X="<<answ<<endl;
+        }
+    }
+
+
+return 0;
+}
+void osszegek(string sztringek ){
+        string elsoResz = sztringek;
+        string szabadtag;
+        bool egyresz;
         bool elojel;
         bool elotag;
         int xosszeg=0;
         int osszeg = 0;
-        // for(int k=0;k<2;k++){
+        if(elsoResz[0]=='-'){elojel=false;elsoResz = elsoResz.substr(1);}
+        else elojel=true;
 
-        if(elsoResz[0]=='-'){elojel=false;    /***A muvelet elso eleme biztos egy szam lesz*/
-                                        elsoResz = elsoResz.substr(1);}
-
-        else                         elojel=true;
 
         int pos1 = elsoResz.find('+');
         int pos2 = elsoResz.find('-');
@@ -41,8 +63,32 @@ int main()
         int szam = 0;
         int xszam=0;
 
+        if(pos1==string::npos && pos2==string::npos){
+            szabadtag = elsoResz;
+            bool volt=false;
+            if(szabadtag.find("x")==string::npos && !volt){
+                for(int i=0;i<szabadtag.length();i++){
+                    int szj = szabadtag[i]-48; szam = szam * 10 + szj;}}
 
-        while (pos1!= -1 || pos2!= -1)
+            if(szabadtag == "x"){xszam = 1;volt = true;}
+            if(szabadtag.find("x")!=string::npos && !volt){
+                                                        int x = szabadtag.find("x");
+                                                        szabadtag.erase(x,1);
+                                                        for(int i=0; i<szabadtag.length(); i++)
+                                                        {
+                                                            int szj = szabadtag[i]-48;
+                                                            xszam = xszam * 10 + szj;}}
+
+                    if(elojel==false){xosszeg-=xszam;
+                                     osszeg-=szam;}
+                    if(elojel==true){xosszeg+=xszam;
+                                    osszeg+=szam;}
+                    szam = 0;
+                    xszam=0;
+        }///ELSO IF....................................................................................
+       else{
+
+         while (pos1!= -1 || pos2!= -1)
         {
 
                                                     if((pos1 < pos2 && pos1!=string::npos && pos2!=string::npos)||(pos2==string::npos && pos1!=string::npos)){
@@ -58,8 +104,7 @@ int main()
                                                     szabadtag = elsoResz.substr(beginning,ending-beginning);   /***Ez a 2 elojel kozotti resz*/
 
 
-                                                    if(szabadtag=="x"){xszam=1;
-                                                    }
+                                                    if(szabadtag=="x"){xszam=1;}
 
                                                     if(szabadtag.find("x")!=-1) {
                                                         int x = szabadtag.find("x");
@@ -113,44 +158,30 @@ int main()
 
 
         }
-
-
-        if(pos1==string::npos && pos2==string::npos)
+          if(pos1==string::npos && pos2==string::npos)
         {
-            int utolsoBetu;
-            if(elsoResz.rfind("M")==string::npos && elsoResz.rfind("P")!=string::npos)utolsoBetu = elsoResz.rfind("P");
-            if(elsoResz.rfind("P")==string::npos && elsoResz.rfind("M")!=string::npos)utolsoBetu = elsoResz.rfind("M");
-            cout<<utolsoBetu;
-
-            /*cout<<"Ez meg vegleg"<<asd<<endl;
-            szabadtag = elsoResz.substr(asd+1);
-            cout<<"Ez a szabadtag:"<<szabadtag<<endl;
-
-            if(szabadtag=="x")xszam;
-            if(szabadtag.find("x")!=-1)
-            {
+            char jel;
+            jel = elsoResz[beginning-1];
+            szabadtag = elsoResz.substr(beginning);
+            bool volt=false;
+            if(szabadtag == "x"){xszam = 1;volt = true;}
+            if(szabadtag.find("x")!=string::npos && !volt){
                 int x = szabadtag.find("x");
                 szabadtag.erase(x,1);
-                cout<<"Ez az uj  szabadtag:"<<szabadtag<<endl;
                 for(int i=0; i<szabadtag.length(); i++)
-                {
-                    int szj = szabadtag[i]-48;
-                    xszam = xszam * 10 + szj;
-                }
-                cout<<"Ez a xszamjegy:"<<xszam<<endl;
-            }
+                                                        {
+                                                            int szj = szabadtag[i]-48;
+                                                            xszam = xszam * 10 + szj;
+                                                        }
 
-            else
-            {
-                for(int i=0; i<szabadtag.length(); i++)
-                {
-                    int szj = szabadtag[i]-48;
-                    szam = szam * 10 + szj;
-                    cout<<"Ez a szamjegy"<<szam<<endl;
+            }
+            if(szabadtag.find("x")==string::npos && !volt){
+                for(int i=0;i<szabadtag.length();i++){
+                    int szj = szabadtag[i]-48; szam = szam * 10 + szj;
                 }
             }
 
-             char jel = elsoResz[asd];
+
                                                         if(jel=='P')
                                                             xosszeg+=xszam;
                                                         if(jel=='P')
@@ -159,19 +190,16 @@ int main()
                                                             xosszeg-=xszam;
                                                         if(jel=='M')
                                                             osszeg-=szam;
+        }}///ELSE....................................................................................................
 
-*/
+
+        if(oldal==0){
+            tomb[0][0]=xosszeg;
+            tomb[0][1]=osszeg;}
+        if(oldal==1){
+            tomb[1][0]=xosszeg;
+            tomb[1][1]=osszeg;
         }
 
-        //elsoResz=masodikResz;
 
-        //}
-
-        cout<<"Az osszeg:"<<osszeg<<endl;
-        cout<<"Az xosszeg:"<<xosszeg<<endl;
-
-    }
-
-
-    return 0;
 }
